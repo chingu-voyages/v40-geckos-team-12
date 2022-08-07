222dfbff953a3ed79354876bda2bfdbc27a358a2
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-07 13:50:04 +0100

    create column

---
+++ frontend/src/components/column/Column.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "../../components";
+import { StyledColumn } from "./Column.styled";
+
+const Column = () => {
+  return (
+    <StyledColumn>
+      <Card />
+    </StyledColumn>
+  );
+};
+
+export default Column;
