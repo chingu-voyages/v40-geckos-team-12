f6e01cce62b890c555367912e95ed795755e38ad
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-07 13:49:22 +0100

    create and style card

---
+++ frontend/src/components/card/Card.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  background-color: #2b2c37;
+  color: #fff;
+  display: grid;
+`;
+
+export const CardTitle = styled.h3`
+  color: #635fc7;
+`;
+
+export const CardDescription = styled.p`
+  color: #828fa3;
+`;
