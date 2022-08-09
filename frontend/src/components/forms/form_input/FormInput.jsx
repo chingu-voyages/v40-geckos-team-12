e99e8920ab8aabb094f8cbd6958391803fdd0a78
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-09 15:22:46 +0100

    create input and label components

---
+++ frontend/src/components/forms/form_input/FormInput.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledInput } from "../Form.styled";
+
+const FormInput = () => {
+  return <StyledInput />;
+};
+
+export default FormInput;
---
+++ frontend/src/components/forms/form_label/FormLabel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LabelValueContainer, StyledLabel, StyledInput } from "../Form.styled";
+
+const FormLabel = ({ text }) => {
+  return <StyledLabel>{text}</StyledLabel>;
+};
+
+export default FormLabel;
