58b8f1486580697d25d5b5af0caf82bb563ddb43
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-09 15:39:41 +0100

    import components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,23 @@
 import Card from "./card/Card";
 import Column from "./column/Column";
+import FormTextArea from "./forms/form_text_area/FormTextArea";
+import FormLabel from "./forms/form_label/FormLabel";
+import FormInput from "./forms/form_input/FormInput";
+import FormSelect from "./forms/form_select/FormSelect";
+import FormOption from "./forms/form_option/FormOption";
+import Form from "./forms/form_component/Form";
+import FormButton from "./forms/form_button/FormButton";
+import FormHeader from "./forms/form_header/FormHeader";
 
-export { Card, Column };
+export {
+  Card,
+  Column,
+  FormTextArea,
+  FormLabel,
+  FormInput,
+  FormSelect,
+  FormOption,
+  Form,
+  FormButton,
+  FormHeader,
+};
