0cc095a4f3ed61520eb6c0270417038520f78c6f
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-09 15:24:30 +0100

    create header component

---
+++ frontend/src/components/forms/form_header/FormHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledFormHeader } from "../Form.styled";
+
+const FormHeader = ({ text }) => {
+  return <StyledFormHeader>{text}</StyledFormHeader>;
+};
+
+export default FormHeader;
