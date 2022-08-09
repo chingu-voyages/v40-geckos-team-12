76ffb41f03bd84193cb8ac21afd185e02da0649b
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-09 15:40:18 +0100

    create text area component

---
+++ frontend/src/components/forms/form_text_area/FormTextArea.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledTextArea } from "../Form.styled";
+
+const FormTextArea = () => {
+  return <StyledTextArea rows="5"></StyledTextArea>;
+};
+
+export default FormTextArea;
