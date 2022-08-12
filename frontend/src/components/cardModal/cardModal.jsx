26c10f6033c6e0200b8c2f83b43a1d681b8d2f3a
MorganBare <morganjbare@gmail.com>
2022-08-12 21:37:04 +0200

    Added Kebab icon and padding to modal container

--- frontend/src/components/cardModal/cardModal.jsx
+++ frontend/src/components/cardModal/cardModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ModalWrapper, ModalContainer, CardTitle, CardDescription } from "./CardModal.styled.js";
+import { ModalWrapper, ModalContainer, TitleKebabContainer, KebabIcon, CardTitle, CardDescription, SubTaskTitle } from "./CardModal.styled.js";
 
 const CardModal = () => {
 
@@ -7,8 +7,12 @@ const CardModal = () => {
         <>
             <ModalWrapper>
                 <ModalContainer>
+                    <TitleKebabContainer>
                     <CardTitle>Research pricing points of various competitors and trial different business models</CardTitle>
+                    <KebabIcon/>
+                    </TitleKebabContainer>
                     <CardDescription>We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.</CardDescription>
+                    <SubTaskTitle>Subtasks (2 of 3)</SubTaskTitle>
                 </ModalContainer>
             </ModalWrapper>
         </>
--- frontend/src/components/cardModal/cardModal.styled.js
+++ frontend/src/components/cardModal/cardModal.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {ReactComponent as Kebab} from '../../assets/icon-vertical-ellipsis.svg';
 
 export const ModalWrapper = styled.div`
   background-color: rgba(151, 151, 151, 0.8);
@@ -14,18 +15,43 @@ export const ModalWrapper = styled.div`
 export const ModalContainer = styled.div`
   background-color: ${(props) => props.theme.main};
   color: #fff;
-  max-width: 400px;
+  max-width: 450px;
   position: relative;
   border-radius: 6px;
   margin: 0 auto;
   top: 50%;
   transform: translateY(-50%);
+  padding: 2em;
+`;
+
+export const KebabIcon = styled(Kebab)`
+  height: 50px;
+  width: 50px;
+`;
+
+
+export const TitleKebabContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const CardTitle = styled.h3`
   color: ${(props) => props.theme.fontColor};
+  font-size: 18px;
 `;
 
 export const CardDescription = styled.p`
   color: ${(props) => props.theme.fontColor};
+  font-size: 13px;
+  color: #828FA3;
+  padding: 1.5em 0;
 `;
+
+export const SubTaskTitle = styled.p`
+  font-size: 13px;
+  color: #828FA3;
+`;
+
+export const CheckboxLabel = styled.div`
+
+`
