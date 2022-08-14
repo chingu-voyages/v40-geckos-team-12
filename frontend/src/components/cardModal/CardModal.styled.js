3e2cdea6c55c4780a63f993fae812e4fa0788e1e
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-14 13:45:26 +0100

    change colors
    When I clicked the card the BG of the modal was not right and the logic for the opacity was back to front.

--- frontend/src/components/cardModal/CardModal.styled.js
+++ frontend/src/components/cardModal/CardModal.styled.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
-import {ReactComponent as Kebab} from '../../assets/icon-vertical-ellipsis.svg';
-import iconCheck from '../../assets/icon-check.svg';
+import { ReactComponent as Kebab } from "../../assets/icon-vertical-ellipsis.svg";
+import iconCheck from "../../assets/icon-check.svg";
 
 export const ModalWrapper = styled.div`
-  background-color: ${(props) => props.theme.modalBackDrop};
-  opacity: ${(props) => props.cardModalToggle ? '0.8' : '1'};
+  /* background-color: ${(props) => props.theme.modalBackDrop}; */
+  background-color: rgba(0, 0, 0, 0.5);
   color: #fff;
   height: 100%;
   width: 100%;
   position: fixed;
-  z-index:1000;
+  z-index: 1000;
   left: 0;
   top: 0;
 `;
 
 export const ModalContainer = styled.div`
-  background-color: ${(props) => props.theme.main};
+  background-color: ${(props) => props.theme.modalBG};
   max-width: 450px;
   position: relative;
   border-radius: 6px;
@@ -72,34 +72,37 @@ export const StyledCheckbox = styled.div`
   display: inline-block;
   width: 16px;
   height: 16px;
-  border: 1px solid #828FA3;
-  background: ${(props) => props.checkActive ? props.theme.fontColorHover : props.theme.main};
-  background-image: ${(props) => props.checkActive ? `url(${iconCheck})` : 'none'};
+  border: 1px solid #828fa3;
+  background: ${(props) =>
+    props.checkActive ? props.theme.fontColorHover : props.theme.main};
+  background-image: ${(props) =>
+    props.checkActive ? `url(${iconCheck})` : "none"};
   background-repeat: no-repeat;
   background-position: center;
   border-radius: 4px;
   transition: all 150ms;
-`
+`;
 
 export const Label = styled.label`
   align-items: center;
   display: flex;
   gap: 8px;
   margin-bottom: 8px;
-  padding: .75em;
+  padding: 0.75em;
   border-radius: 5px;
   background-color: ${(props) => props.theme.subTaskBG};
 
   &:hover {
     background-color: ${(props) => props.theme.labelHover};
   }
-`
+`;
 
 export const LabelText = styled.span`
-color: ${(props) => props.checkActive ? props.theme.secondFontColor : props.theme.fontColor};
-font-size: 13px;
-width: 100%;
-text-decoration: ${(props) => props.checkActive ? 'line-through' : 'none'};
+  color: ${(props) =>
+    props.checkActive ? props.theme.secondFontColor : props.theme.fontColor};
+  font-size: 13px;
+  width: 100%;
+  text-decoration: ${(props) => (props.checkActive ? "line-through" : "none")};
 `;
 
 export const StatusContainer = styled.div``;
@@ -114,7 +117,7 @@ export const StyledLabel = styled.label`
 
 export const StyledSelect = styled.select`
   width: 100%;
-  padding: .75em;
+  padding: 0.75em;
   margin-top: 0.5rem;
   border-radius: 4px;
   &:focus {
