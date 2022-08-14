f4b24e6b515f479cbc42406de617073fa1c8e7a3
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-14 13:41:57 +0100

    send the object upwards
    So here, I have an onclick and I also know that since this component is being rendered from a loop, whatever card I click will be unique. Since it is unique I can pass it in a function and use the data in the modal. That way whatever modal is clicked will have the right data.

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { CardContainer, CardTitle, CardDescription } from "./Card.styled.js";
 
-const Card = ({handleCardModalToggle}) => {
+const Card = ({ handleCardModalToggle, task }) => {
   return (
     <>
-      <CardContainer onClick={handleCardModalToggle}>
-        <CardTitle>Ut porttitor orci nec eros maximus bibendum. Sed lobortis massa a rutrum tincidunt.</CardTitle>
-        <CardDescription>Aliquam erat volutpat. Fusce congue est et ante venenatis facilisis. In vestibulum dui velit, at feugiat leo tempus at. Quisque quis pellentesque neque.</CardDescription>
+      <CardContainer onClick={() => handleCardModalToggle(task)}>
+        <CardTitle>{task.title}</CardTitle>
+        <CardDescription>{task.description}</CardDescription>
       </CardContainer>
     </>
   );
