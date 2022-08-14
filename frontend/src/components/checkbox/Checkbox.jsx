0c86ee1f626388c85833a90f0a94e88035ae550b
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-14 13:36:41 +0100

    remove map
    Because I have changed some of the logic, the map is no longer needed.

--- frontend/src/components/checkbox/Checkbox.jsx
+++ frontend/src/components/checkbox/Checkbox.jsx
@@ -1,33 +1,21 @@
-import React from 'react'
-import { 
-    HiddenCheckBox, 
-    StyledCheckbox, 
-    LabelText, 
-    Label, 
+import React from "react";
+import {
+  HiddenCheckBox,
+  StyledCheckbox,
+  LabelText,
+  Label,
 } from "../cardModal/CardModal.styled";
 
-
-const Checkbox = ({task, handleCheck, checked}) => {
-
-const subtasks = task.subtasks;
-
-
+const Checkbox = ({ subtask, handleCheck, checked }) => {
   return (
-   <>    
-   {subtasks.length && subtasks.map((subtask, index) => {
-    return (
-     <Label key={index}>
-        <HiddenCheckBox onChange={handleCheck} value={subtask} />
-        <StyledCheckbox checkActive={checked.includes(subtask) ? true : false} />
-        <LabelText checkActive={checked.includes(subtask) ? true : false} >{subtask}</LabelText>
+    <Label>
+      <HiddenCheckBox onChange={handleCheck} value={subtask} />
+      <StyledCheckbox checkActive={checked.includes(subtask) ? true : false} />
+      <LabelText checkActive={checked.includes(subtask) ? true : false}>
+        {subtask}
+      </LabelText>
     </Label>
-    )
-   })}
-   
-  
-    </>
+  );
+};
 
-  )
-}
-
-export default Checkbox
+export default Checkbox;
