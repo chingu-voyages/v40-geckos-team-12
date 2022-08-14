a6ade1257d8a0eac3d80c828141a4a261af461df
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-14 13:44:27 +0100

    lots of red
    So most of the red you see here is not what I have deleted, it is because I had a code formatted plugin.
    
    Remember that dispatch sends an >>> action <<<, and an action is a function.
    
    Here you are trying to send an action as well as another function called setCurrentTask.

--- frontend/src/components/cardModal/CardModal.jsx
+++ frontend/src/components/cardModal/CardModal.jsx
@@ -1,82 +1,101 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { CheckBox } from "../../components";
-import { selectTasks, move_from_todo_to_doing, move_from_doing_to_done } from "features/slices/tasksSlice/tasksSlice";
+import {
+  selectTasks,
+  move_from_todo_to_doing,
+  move_from_doing_to_done,
+} from "features/slices/tasksSlice/tasksSlice";
 
-import { 
-    ModalWrapper, 
-    ModalContainer, 
-    TitleKebabContainer, 
-    KebabIcon, 
-    CardTitle, 
-    CardDescription, 
-    SubTaskContainer, 
-    SubTaskTitle,  
-    StatusContainer, 
-    StyledLabel, 
-    StyledSelect, 
-    StyledOption, 
+import {
+  ModalWrapper,
+  ModalContainer,
+  TitleKebabContainer,
+  KebabIcon,
+  CardTitle,
+  CardDescription,
+  SubTaskContainer,
+  SubTaskTitle,
+  StatusContainer,
+  StyledLabel,
+  StyledSelect,
+  StyledOption,
 } from "./CardModal.styled.js";
 
-const CardModal = ({handleCardModalToggle, cardModalToggle}) => {
+const CardModal = ({ handleCardModalToggle, cardModalToggle, modalTask }) => {
+  // Gives us an array of task objects
+  const tasks = useSelector(selectTasks);
+  // Redux
+  const dispatch = useDispatch();
+  // Set state based on a specific card/task
 
-// Gives us an array of task objects
-const tasks = useSelector(selectTasks);
-// Redux 
-const dispatch = useDispatch();
-// Set state based on a specific card/task
-const [currentTask, setCurrentTask] = React.useState(tasks[0]);
-// updates the task.status state when a dropdown option is selected and dispatches it
-const handleChange = (e) => {
+  const [currentTask, setCurrentTask] = React.useState(modalTask);
+
+  // updates the task.status state when a dropdown option is selected and dispatches it
+  const handleChange = (e) => {
     e.preventDefault();
-    if(e.target.value === 'Doing'){
-        dispatch(move_from_todo_to_doing(setCurrentTask({...currentTask, status: e.target.value})))
-    } else if (e.target.value === 'Done'){
-        dispatch(move_from_doing_to_done(setCurrentTask({...currentTask, status: e.target.value})))
+    if (e.target.value === "Doing") {
+      setCurrentTask({ ...currentTask, status: e.target.value });
+
+      dispatch(move_from_todo_to_doing(currentTask.id));
+    } else if (e.target.value === "Done") {
+      setCurrentTask({ ...currentTask, status: e.target.value });
+      dispatch(move_from_doing_to_done(currentTask.id));
     }
-};
+  };
 
-// state for the checkboxes
-const [checked, setChecked] = React.useState([]);
-// handles the checkboxes state
-function handleCheck(event){
+  // state for the checkboxes
+  const [checked, setChecked] = React.useState([]);
+  // handles the checkboxes state
+  function handleCheck(event) {
     let subTaskStatus = [...checked];
-    if (event.target.checked) {
-        subTaskStatus = [...checked, event.target.value];
-      } else {
-        subTaskStatus.splice(checked.indexOf(event.target.value), 1);
-      }
-      setChecked(subTaskStatus);
-}    
 
-    return (
-        <>
-            <ModalWrapper cardModalToggle={cardModalToggle} onClick={handleCardModalToggle}>
-                <ModalContainer onClick={e => e.stopPropagation()} >
-                    <TitleKebabContainer>
-                    <CardTitle>{currentTask.title}</CardTitle>
-                    <KebabIcon/>
-                    </TitleKebabContainer>
-                    <CardDescription>{currentTask.description}</CardDescription>
-                    <SubTaskTitle>Subtasks ({checked.length} of {currentTask.subtasks.length})</SubTaskTitle>
-                    <SubTaskContainer>
-                        {tasks.map((task, index) => <CheckBox task={task} key={index} handleCheck={handleCheck} checked={checked} />)}
-                    </SubTaskContainer>
-                    <StatusContainer>
-                        <StyledLabel text="Status">Current Status</StyledLabel>
-                        <StyledSelect
-                        value={currentTask.status}
-                        onChange={handleChange}
-                        >
-                            <StyledOption>Todo</StyledOption>
-                            <StyledOption>Doing</StyledOption>
-                            <StyledOption>Done</StyledOption>
-                        </StyledSelect>
-                    </StatusContainer>
-                </ModalContainer>
-            </ModalWrapper>
-        </>
-    )
+    if (event.target.checked) {
+      subTaskStatus = [...checked, event.target.value];
+    } else {
+      subTaskStatus.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(subTaskStatus);
+  }
+
+  return (
+    <>
+      <ModalWrapper
+        cardModalToggle={cardModalToggle}
+        onClick={handleCardModalToggle}
+      >
+        <ModalContainer onClick={(e) => e.stopPropagation()}>
+          <TitleKebabContainer>
+            <CardTitle>{currentTask.title}</CardTitle>
+            <KebabIcon />
+          </TitleKebabContainer>
+          <CardDescription>{currentTask.description}</CardDescription>
+          <SubTaskTitle>
+            Subtasks ({currentTask.subtasks.length} of
+            {currentTask.subtasks.length})
+          </SubTaskTitle>
+          <SubTaskContainer>
+            {currentTask.subtasks?.map((subtask, index) => (
+              <CheckBox
+                subtask={subtask}
+                key={index}
+                handleCheck={handleCheck}
+                checked={checked}
+              />
+            ))}
+          </SubTaskContainer>
+          <StatusContainer>
+            <StyledLabel text="Status">Current Status</StyledLabel>
+            <StyledSelect value={currentTask.status} onChange={handleChange}>
+              <StyledOption>Todo</StyledOption>
+              <StyledOption>Doing</StyledOption>
+              <StyledOption>Done</StyledOption>
+            </StyledSelect>
+          </StatusContainer>
+        </ModalContainer>
+      </ModalWrapper>
+    </>
+  );
 };
 
 export default CardModal;
