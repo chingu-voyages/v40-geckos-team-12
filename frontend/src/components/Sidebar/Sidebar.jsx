7061a616008e16f674b4d199d7b07a5012c7ff43
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-17 08:00:42 +0100

    prettier formatted the code

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { NavContainer, ListContainer, List, Brand } from './Sidebar.styled'
-import logo from '../../assets/logo-dark.svg'
+import React from "react";
+import { NavContainer, ListContainer, List, Brand } from "./Sidebar.styled";
+import logo from "../../assets/logo-dark.svg";
 import { useSelector, useDispatch } from "react-redux";
-import { toggleTheme, selectIsLightTheme } from "../../features/slices/themeSlice/themeSlice";
-
+import {
+  toggleTheme,
+  selectIsLightTheme,
+} from "../../features/slices/themeSlice/themeSlice";
 
 const Sidenav = () => {
   const dispatch = useDispatch();
   const isLightTheme = useSelector(selectIsLightTheme);
   return (
-    <NavContainer>
-        <Brand><img src={logo} style={{color: 'black'}} alt="" /></Brand>
-        <ListContainer>
-            <List>Platform Launch</List>
-            <List>Marketing Plan</List>
-            <List>Roadmap</List>
-            <button onClick={() => dispatch(toggleTheme())}>Toggle</button>
-        </ListContainer>
-    </NavContainer>
-  )
-}
+    <>
+      <Brand>
+        <img src={logo} style={{ color: "black" }} alt="" />
+      </Brand>
+      <ListContainer>
+        <List>Platform Launch</List>
+        <List>Marketing Plan</List>
+        <List>Roadmap</List>
+        <button onClick={() => dispatch(toggleTheme())}>Toggle</button>
+      </ListContainer>
+    </>
+  );
+};
 
-export default Sidenav
+export default Sidenav;
