82742835a9740c8f2ebecdd51a6d7faa85ae049e
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-17 09:06:45 +0100

    merge main into branch / resolve conflicts

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,10 @@
 import Form from "../components/forms/form_component/Form";
 import Card from "./card/Card";
 import Column from "./column/Column";
+import ListItem from "./Sidebar/sideBarListItem/ListItem";
 
 import SidebarContainer from "./SidebarContainer/SidebarContainer";
 
 import CardModal from "./cardModal/CardModal";
 import CheckBox from "./checkbox/Checkbox";
-export { Card, Column, CardModal, Form, CheckBox, SidebarContainer };
+export { Card, Column, CardModal, Form, CheckBox, SidebarContainer, ListItem };
