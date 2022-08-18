dfa58c7adf456c2fa88a2895f1afa5110d96871a
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-18 09:40:31 +0100

    NO MORE FIXED POSITION!
    Please don't add this back.

--- frontend/src/components/SidebarContainer/sidebarContainer.styled.js
+++ frontend/src/components/SidebarContainer/sidebarContainer.styled.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: ${(props) => props.theme.main};
   padding: 20px 15px;
-  height: 100vh;
+  min-height: 100vh;
   width: 300px;
   display: block;
   flex: 1 0 300px;
-  position: fixed;
+  /* position: fixed; */
   z-index: 1; /* Stay on top */
   top: 0; /* Stay at the top */
   left: 0;
