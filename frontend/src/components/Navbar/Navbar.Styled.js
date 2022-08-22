df9b9e8130cb0154368a4ef43a9ca9dc81b572fc
Morgan <morganjbare@gmail.com>
2022-08-22 12:03:14 +0200

    Reduced padding on mobile button. Increased width of kebab icon as it was being eaten by the outer div.

--- frontend/src/components/Navbar/Navbar.Styled.js
+++ frontend/src/components/Navbar/Navbar.Styled.js
@@ -7,7 +7,7 @@ export const NavContainer = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 20px;
+  padding: 20px;
   border-bottom: 2px solid ${(props) => props.theme.subTaskBG};
 
   @media (max-width: 770px){
@@ -33,14 +33,14 @@ export const NavButtonWrapper = styled.div`
 
 export const NavButton = styled.button`
   width: 165px;
-  padding: 13px 24px 13px 24px;
+  padding: 1em 1.5em;
   color: #ffffff;
   font-weight: 700;
   font-size: 15px;
   line-height: 19px;
   background: #635fc7;
   border-radius: 24px;
-  margin-right: 24px;
+  margin-right: 1em;
   cursor: pointer;
   width: 100%;
   border: none;
@@ -49,13 +49,19 @@ export const NavButton = styled.button`
     background-color: #A8A4FF;
     transition: background-color 0.4s ease;
   }
+
+  @media (max-width: 630px){
+    padding: .5em 1em;
+  }
 `;
 
 export const NavMenuIcon = styled.span`
   color: black;
 `;
 
-export const KebabIcon = styled(Kebab)``;
+export const KebabIcon = styled(Kebab)`
+  
+`;
 
 export const PlusIcon = styled(Plus)`
   @media (min-width: 635px){
