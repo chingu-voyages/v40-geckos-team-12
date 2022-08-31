c77ba44e4ac8c774640bc069383bdbbc5db70088
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-08-31 09:19:46 +0100

    setup protected routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Login, Register } from "./components";
+import { ProtectedRoute } from "./components";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
+
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
